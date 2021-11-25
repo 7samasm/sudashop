dc12ac1243e1c2a784888ef576996bb212772896
Hussam Abdallah <hussamasm92@gmail.com>
2021-11-25 02:58:41 +0200

    migrate to clever mdb

--- server/routes/section.js
+++ server/routes/section.js
@@ -1,14 +1,13 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 /*  Routes to define development "kitchen sink" samples                                           */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-const express = require('express')
-const isAuth = require('../middlewares/routes_middlewares/isAuth')
-const sectionController  = require('../controllers/section')
+const express = require("express");
+const isAuth = require("../middlewares/routes_middlewares/isAuth");
+const sectionController = require("../controllers/section");
 
-const router = express.Router() // router middleware for koa
+const router = express.Router(); // router middleware for koa
 
-router.get('/',sectionController.getAll)
-router.post('/',isAuth,sectionController.addSection);
+router.get("/", sectionController.getAll);
+router.post("/", isAuth(), sectionController.addSection);
 
-
-module.exports = router
+module.exports = router;
--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,39 +1,40 @@
-const querystring = require('querystring')
-const axios = require('axios')
-const jwtDecode = require('jwt-decode')
-const {validationResult} = require('express-validator')
-const consts = require('../utils/consts')
+const querystring = require("querystring");
+const axios = require("axios");
+const jwtDecode = require("jwt-decode");
+const { validationResult } = require("express-validator");
+const consts = require("../utils/consts");
 
-const decode = (token) => {
-  return token ? jwtDecode(token) : null
-}
+const decode = token => {
+  return token ? jwtDecode(token) : null;
+};
 
 /**
  * Handle possibility where token endpoint, at exp returns seconds instead of µ seconds
  */
-const handleTokenExp = (exp) => {
-  let out = exp
+const handleTokenExp = exp => {
+  let out = exp;
 
-  const milliseconds = new Date().getTime()
+  const milliseconds = new Date().getTime();
   // const millisecondsDigitCount = ((milliseconds).toString()).length
-  const seconds = Math.floor(milliseconds / 1000)
-  const secondsDigitCount = ((seconds).toString()).length
+  const seconds = Math.floor(milliseconds / 1000);
+  const secondsDigitCount = seconds.toString().length;
 
-  const isExpressedInSeconds = ((exp).toString()).length === secondsDigitCount
+  const isExpressedInSeconds = exp.toString().length === secondsDigitCount;
   // const isExpressedInMilliSeconds = ((exp).toString()).length === millisecondsDigitCount
 
   // If the exp is 25 hours or less, adjust the time to miliseconds
   // Otherwise let's not touch it
   if (isExpressedInSeconds) {
-    const durationInSeconds = Math.floor((exp - seconds))
-    const hours = Math.floor(durationInSeconds / 3600)
-    if (hours < 25) { // Make 25 configurable?
-      out *= 1000
+    const durationInSeconds = Math.floor(exp - seconds);
+    const hours = Math.floor(durationInSeconds / 3600);
+    if (hours < 25) {
+      // Make 25 configurable?
+      out *= 1000;
     }
   }
 
-  return out
-}
+  return out;
+};
 
 /**
  * Make an async off-the-band POST request.
@@ -48,77 +49,78 @@ const handleTokenExp = (exp) => {
  * to point to your own API.
  */
 const createRequest = async (method, url, requestConfig) => {
-  const {
-    payload = null,
-    ...restOfRequestConfig
-  } = requestConfig
+  const { payload = null, ...restOfRequestConfig } = requestConfig;
   const requestConfigObj = {
     timeout: consts.AXIOS_DEFAULT_TIMEOUT,
     baseURL: consts.LB_ADDR,
     method,
     url,
     ...restOfRequestConfig
-  }
+  };
   if (payload !== null) {
-    requestConfigObj.data = querystring.stringify(payload)
+    requestConfigObj.data = querystring.stringify(payload);
   }
 
-  const recv = await axios.request(requestConfigObj)
-  const data = Object.assign({}, recv.data)
+  const recv = await axios.request(requestConfigObj);
+  const data = Object.assign({}, recv.data);
 
-  return Promise.resolve(data)
-}
+  return Promise.resolve(data);
+};
 
-const getUserData = async (token) => {
-  const userinfo = [
-    'DisplayName',
-    'PreferredLanguage',
-    'TimeZone'
-  ]
+const getUserData = async token => {
+  const userinfo = ["DisplayName", "PreferredLanguage", "TimeZone"];
   const params = {
     Token: token,
-    userinfo: userinfo.join(',')
-  }
+    userinfo: userinfo.join(",")
+  };
 
   /**
    * Would create a request like this;
    *
    *     GET /platform/uaano/oauth/validate?Token=111.222.333&userinfo=PreferredLanguage,TimeZone
    */
-  const response = await createRequest('GET', consts.ENDPOINT_BACKEND_VALIDATE, { params })
+  const response = await createRequest(
+    "GET",
+    consts.ENDPOINT_BACKEND_VALIDATE,
+    { params }
+  );
 
   const body = {
     status: response.Status
-  }
-  body.UserInfo = response.UserInfo || {}
+  };
+  body.UserInfo = response.UserInfo || {};
 
-  return Promise.resolve(body)
-}
+  return Promise.resolve(body);
+};
 
-
-const emitErrors = (ctx,error) => {
-  ctx.status = error.status || 500
-  ctx.body = error.message
-  ctx.app.emit('error',error,ctx);
-}
+const emitErrors = (ctx, error) => {
+  ctx.status = error.status || 500;
+  ctx.body = error.message;
+  ctx.app.emit("error", error, ctx);
+};
 
 const getEnvironmentVariable = variable => process.env[variable];
-const getGlobalDBUrl = () => `mongodb+srv://${getEnvironmentVariable(consts.DB_USERNAME)}:${getEnvironmentVariable(consts.DB_PASSWORD)}@cluster0.ldcad.mongodb.net/${getEnvironmentVariable(consts.DB)}?retryWrites=true&w=majority`
-const getLocalDBUrl = () => `mongodb://localhost:27017/${getEnvironmentVariable(consts.DB)}`
+const getGlobalDBUrl = () =>
+  `mongodb://${process.env[consts.DB_USERNAME]}:${
+    process.env[consts.DB_PASSWORD]
+  }@${process.env[consts.DB]}-mongodb.services.clever-cloud.com:27017/${
+    process.env[consts.DB]
+  }`;
+const getLocalDBUrl = () =>
+  `mongodb://localhost:27017/${process.env[consts.DB]}`;
 
-
-const validResult = (req) =>  {
-  const errs  = validationResult(req) ;
+const validResult = req => {
+  const errs = validationResult(req);
   if (!errs.isEmpty()) {
     for (const err of errs.array()) {
-      const e  = new Error(`${err.msg} in ${err.param} input!`)
-      e.statusCode = 422
-      e.data = errs.array()
-      console.log(e)
-      throw e
+      const e = new Error(`${err.msg} in ${err.param} input!`);
+      e.statusCode = 422;
+      e.data = errs.array();
+      console.log(e);
+      throw e;
     }
   }
-}
+};
 
 module.exports = {
   decode,
@@ -130,4 +132,4 @@ module.exports = {
   getGlobalDBUrl,
   getLocalDBUrl,
   validResult
-}
+};
